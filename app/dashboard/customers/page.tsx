6db685d300f98dfc139babd004f5d1c0e652d146
d10labs <ricardomedios@outlook.com>
2024-05-08 17:08:46 -0600

    Working Version Chapter 16 - Adding Metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers Metadata Test',
+};
+
+
 export default function Page() {
     return <p>Customers Page</p>;
   }
